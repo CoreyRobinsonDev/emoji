bace656941b08536108b4ca92a2ea595e5d5b579
CoreyRobinsonDev <coreyrob52@gmail.com>
2025-05-11 19:57:23 -0400

    fix: remove top level await

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
+import { readFileSync } from "fs"
 import type { Emojis } from "./types.ts"
 export * from "./types.ts"
 export * from "./functions.ts"
-export const emojis: Emojis = await Bun.file("./src/emoji.json").json()
+export const emojis: Emojis = JSON.parse(readFileSync("./src/emoji.json", "utf-8"))
