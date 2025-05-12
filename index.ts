7be0690813b5c6052eba9cf8ed29ddf08173f217
CoreyRobinsonDev <coreyrob52@gmail.com>
2025-05-11 20:08:07 -0400

    refactor: move index to project root

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from "fs"
-import type { Emojis } from "./types.ts"
-export * from "./types.ts"
-export * from "./functions.ts"
+import type { Emojis } from "./src/types.ts"
+export * from "./src/types.ts"
+export * from "./src/functions.ts"
 export const emojis: Emojis = JSON.parse(readFileSync("./src/emoji.json", "utf-8"))
